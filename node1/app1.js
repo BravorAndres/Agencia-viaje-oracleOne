2609b001fc9bbf24bfbf47f744c28856c29052ac
Andrew <bravor.andres24@gmail.com>
2022-11-15 20:12:00 -0500

    First Commit

---
+++ node1/app1.js
@@ -0,0 +1,19 @@
+
+
+let nombre;
+
+var b = "holli";
+//con let se declaran las variables de manera encapsulada dentro de la funcion correspondiente
+
+//con var se declaran de manera global
+
+function myFunc(){
+    let a = 32;
+    console.log(b);
+
+    //para escribir variables en consola
+    console.log('la variable es ${a}');// no como
+
+}
+
+console.log("hola mundo");
